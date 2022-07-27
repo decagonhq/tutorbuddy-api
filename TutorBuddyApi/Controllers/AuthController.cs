b5f8345fb1fb7c74985f89378a2b8fba95b07324
Louis Otu <otulouisakhigbe@gmail.com>
2022-07-27 06:50:11 +0100

    Postgres persistent issue fix

---
+++ TutorBuddyApi/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TutorBuddyApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
