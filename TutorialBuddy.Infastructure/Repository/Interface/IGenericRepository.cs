3ebac53165c971dc3dd6361d0a47e9c6c52f42ad
Mac <mac@Macs-MacBook-Pro-2.local>
2022-07-28 12:24:35 +0100

    add generic interface

---
+++ TutorialBuddy.Infastructure/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TutorBuddy.Infrastructure.Repository.Interface
+{
+	public interface IGenericRepository<T> where T : class
+    {
+        Task<bool> Add(T entity);
+        Task<bool> AddRange(IEnumerable<T> entities);
+        Task<bool> Delete(T entity);
+        Task<bool> DeleteRange(IEnumerable<T> entities);
+        Task<IEnumerable<T>> GetAllRecord();
+        Task<T> GetARecord(string Id);
+        Task<bool> Update(T entity);
+        Task<bool> UpdateRange(IEnumerable<T> entities);
+    }
+}
+
