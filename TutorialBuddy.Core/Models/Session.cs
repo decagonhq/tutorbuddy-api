21fa8dff2691488ad1261b65f6ebfa4c732139c1
EmmanuelHarry <emmanuel.harry@decagon.dev>
2022-08-29 15:00:06 +0100

    updated

--- TutorialBuddy.Core/Models/Session.cs
+++ TutorialBuddy.Core/Models/Session.cs
@@ -10,9 +10,15 @@ namespace TutorBuddy.Core.Models
     public class Session: BaseEntity
     {
         public TutorSubject TutorSubject { get; set; }
+        public TutorComment TutorComment { get; set; }
+        public StudentComment StudentComment { get; set; }
         public User Student { get; set; }
+        public Tutor Tutor { get; set; }
+        public int RateTutor { get; set; }
+        public int RateStudent { get; set; }
         public DateTime Startime { get; set; }
         public DateTime EndTime { get; set; }
         public SessionStatus Status { get; set; }
+        public int RatingStudentCount { get; internal set; }
     }
 }
